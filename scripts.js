889065057a7225ceab88df4a0b93e509700d121b
Nkaka Yves <nkakayves515@gmail.com>
2020-10-05 07:48:14 +0200

    adding javascript toggle

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-    $("ima").click(function(){
-      $("pt").toggle();
-    });
+jQuery(document).ready(function() {
+  jQuery("button").click(function() {
+    alert("Thank you Your feedback is recorded");
   });
+  $(".hid1").click(function() {
+    $(".dis1").toggle();
+    $(".hid1").toggle();
+  });
+  $(".hid2").click(function() {
+    $(".dis2").toggle();
+    $(".hid2").toggle();
+  });
+  $(".hid3").click(function() {
+    $(".dis3").toggle();
+    $(".hid3").toggle();
+  });
+});
